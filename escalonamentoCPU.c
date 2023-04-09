cba6df397de288a7f555536f8e169766f4ec4e70
Marco Antonio <dev.10@pbhub.com.br>
2023-04-08 22:35:31 -0300

    SJF

--- escalonamentoCPU.c
+++ escalonamentoCPU.c
@@ -4,28 +4,29 @@
 
 #define RR_QUANTUM 2
 
-int leArquivo();
-
-void FCFS();
-void calculaTempoEsperaFCFS();
-void calculaTempoRetornoFCFS();
-void calculaTempoRespostaFCFS();
-
-void SJF();
-void calculaTempoEsperaSJF();
-void calculaTempoRetornoSJF();
-void calculaTempoRespostaSJF();
-
 typedef struct processos
 {
     int tempoChegada;
     int tempoDuracao;
     int tempoInicio;
     int tempoFim;
+    int tempoExecucao;
     int tempoResposta;
     int tempoRetorno;
     int tempoEspera;
 } Processo;
+
+int leArquivo();
+
+void FCFS();
+void calculaTempoEsperaFCFS(Processo *aux, int *tempoEspera);
+void calculaTempoRetornoFCFS(Processo *aux, int *tempoRetorno);
+void calculaTempoRespostaFCFS(Processo *aux, int *tempoResposta);
+
+void SJF();
+void calculaTempoEsperaSJF(Processo *aux, int *tempoEspera);
+void calculaTempoRetornoSJF(Processo *aux, int *tempoRetorno);
+void calculaTempoRespostaSJF(Processo *aux, int *tempoResposta);
 
 Processo processos[100] = {0};
 float    NUM_PROCESSOS  = 0.0;
@@ -67,48 +68,145 @@ int leArquivo()
 }
 
 // FCFS - First Come First Served
-// ex saida: FCFS {retornoM} {respostaM} {esperaM}
 void FCFS()
 {
+    Processo* aux       = NULL;
     int tempoTotal      = 0;
     int tempoRetorno    = 0;
     int tempoResposta   = 0;
     int tempoEspera     = 0;
-
-    processos[0].tempoInicio = 0;
-    processos[0].tempoFim    = processos[0].tempoDuracao;
-
-    for(int i = 1; i < NUM_PROCESSOS; i++) processos[i].tempoInicio = processos[i-1].tempoInicio + processos[i-1].tempoDuracao;
-    for(int i = 1; i < NUM_PROCESSOS; i++) processos[i].tempoFim    = processos[i].tempoInicio + processos[i].tempoDuracao;
     
-    calculaTempoEsperaFCFS();
-    calculaTempoRetornoFCFS();
-    calculaTempoRespostaFCFS();
+    aux = malloc(sizeof(Processo) * NUM_PROCESSOS);
+    if(!aux) return;
+    
+    memcpy(aux, processos, sizeof(Processo) * NUM_PROCESSOS);
 
+    
+    aux[0].tempoInicio = 0;
+    aux[0].tempoFim    = aux[0].tempoDuracao;
 
-    for(int i = 0; i < NUM_PROCESSOS; i++)
+    for(int i = 1; i < NUM_PROCESSOS; i++) aux[i].tempoInicio = aux[i-1].tempoInicio + aux[i-1].tempoDuracao;
+    for(int i = 1; i < NUM_PROCESSOS; i++) aux[i].tempoFim    = aux[i].tempoInicio   + aux[i].tempoDuracao;
+    
+    calculaTempoEsperaFCFS(aux, &tempoEspera);
+    calculaTempoRetornoFCFS(aux, &tempoRetorno);
+    calculaTempoRespostaFCFS(aux, &tempoResposta);
+
+    printf("FCFS %.1f %.1f %.1f\n", tempoRetorno/NUM_PROCESSOS, tempoResposta/NUM_PROCESSOS, tempoEspera/NUM_PROCESSOS);
+}
+
+void calculaTempoEsperaFCFS(Processo *aux, int *tempoEspera)
+{
+    for(int i = 0; i != NUM_PROCESSOS; i++) 
     {
-        tempoRetorno  += processos[i].tempoRetorno;
-        tempoResposta += processos[i].tempoResposta;
-        tempoEspera   += processos[i].tempoEspera;
+        aux[i].tempoEspera = aux[i].tempoInicio - aux[i].tempoChegada;
+        *tempoEspera += aux[i].tempoEspera;
+    }
+}
+
+void calculaTempoRetornoFCFS(Processo *aux, int *tempoRetorno)
+{
+    for(int i = 0; i != NUM_PROCESSOS; i++)
+    {
+        aux[i].tempoRetorno = aux[i].tempoFim - aux[i].tempoChegada;
+        *tempoRetorno += aux[i].tempoRetorno;
+    } 
+}
+
+void calculaTempoRespostaFCFS(Processo *aux, int *tempoResposta)
+{
+    for(int i = 0; i != NUM_PROCESSOS; i++)
+    {
+        aux[i].tempoResposta = aux[i].tempoInicio - aux[i].tempoChegada;
+        *tempoResposta += aux[i].tempoResposta;
+    } 
+}
+
+// SJF - Shortest Job First
+void SJF()
+{
+    Processo* aux       = NULL;
+    int tempoTotal      = 0;
+    int tempoRetorno    = 0;
+    int tempoResposta   = 0;
+    int tempoEspera     = 0;
+    int tempoAtual      = 0;
+    int restantes       = NUM_PROCESSOS;
+
+    aux = malloc(sizeof(Processo) * NUM_PROCESSOS);
+    if(!aux) return;
+
+    memcpy(aux, processos, sizeof(Processo) * NUM_PROCESSOS);
+    int *duracao = NULL;
+
+    while(restantes > 0)
+    {
+        int proximo = -1;
+        int duracaoProx = -1;
+
+        for(int i = 0; i < NUM_PROCESSOS; i++)
+        {
+            int duracaoAuxI = aux[i].tempoDuracao;
+
+            if(aux[i].tempoChegada <= tempoAtual && duracaoAuxI > 0)
+            {
+                if(proximo == -1 || duracaoAuxI < duracaoProx)
+                {
+                    proximo = i;
+                    duracaoProx = duracaoAuxI;
+                }
+            }
+        }
+
+        if(proximo == -1) tempoAtual++;
+        else
+        {
+            tempoAtual++;
+            
+            aux[proximo].tempoInicio = tempoAtual;
+            aux[proximo].tempoExecucao++;
+            aux[proximo].tempoDuracao--;
+
+            if(aux[proximo].tempoDuracao == 0)
+            {
+                aux[proximo].tempoFim = tempoAtual;
+                restantes--;
+            }
+        }
     }
 
-    printf("FCFS %.2f %.2f %.2f\n", tempoRetorno/NUM_PROCESSOS, tempoResposta/NUM_PROCESSOS, tempoEspera/NUM_PROCESSOS);
+    calculaTempoEsperaSJF(aux, &tempoEspera);
+    calculaTempoRetornoSJF(aux, &tempoRetorno);
+    calculaTempoRespostaSJF(aux, &tempoResposta);
+
+    printf("SJF %.1f %.1f %.1f\n", tempoRetorno/NUM_PROCESSOS, tempoResposta/NUM_PROCESSOS, tempoEspera/NUM_PROCESSOS);
 }
 
-void calculaTempoEsperaFCFS()
+void calculaTempoEsperaSJF(Processo *aux, int *tempoEspera)
 {
-    for(int i = 0; i != NUM_PROCESSOS; i++) processos[i].tempoEspera = processos[i].tempoInicio - processos[i].tempoChegada;
+    for(int i = 0; i != NUM_PROCESSOS; i++)
+    {
+        aux[i].tempoEspera = aux[i].tempoInicio - aux[i].tempoChegada - aux[i].tempoExecucao;
+        *tempoEspera += aux[i].tempoEspera;
+    } 
 }
 
-void calculaTempoRetornoFCFS()
+void calculaTempoRetornoSJF(Processo *aux, int *tempoRetorno)
 {
-    for(int i = 0; i != NUM_PROCESSOS; i++) processos[i].tempoRetorno = processos[i].tempoFim - processos[i].tempoChegada;
+    for(int i = 0; i != NUM_PROCESSOS; i++)
+    {
+        aux[i].tempoRetorno = aux[i].tempoFim - aux[i].tempoChegada;
+        *tempoRetorno += aux[i].tempoRetorno;
+    } 
 }
 
-void calculaTempoRespostaFCFS()
+void calculaTempoRespostaSJF(Processo *aux, int *tempoResposta)
 {
-    for(int i = 0; i != NUM_PROCESSOS; i++) processos[i].tempoResposta = processos[i].tempoInicio - processos[i].tempoChegada;
+    for(int i = 0; i != NUM_PROCESSOS; i++)
+    {
+        aux[i].tempoResposta = aux[i].tempoFim - aux[i].tempoChegada - aux[i].tempoExecucao;
+        *tempoResposta += aux[i].tempoResposta;
+    } 
 }
 
 int main()
@@ -126,7 +224,7 @@ int main()
     }
 
     FCFS();
-    // SJF();
+    SJF();
     // RR();
 
     return 0;
